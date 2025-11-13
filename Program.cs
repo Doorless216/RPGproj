a187eaa2705b89b46ae668088c001b986667bfe4
gabriel morales uribe <gabrielmoralesuribe530@gmail.com>
2025-11-12 21:36:57 -0500

    Setup-basic-layout

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace RPGproj;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        player player = new player("gabe", 100);
+        player.Inventory = new Inventory();
+
+        player.Inventory.AddItem("sword");
+
+        player.DisplayPlayerInfo();
+        player.Inventory.RemoveItem("sword");
+        player.DisplayPlayerInfo();
+    }
+}
